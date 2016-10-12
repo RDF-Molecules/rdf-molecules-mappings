b525a5ccb79ae47feb204ad8524e351fd5b9c253
Luigi Selmi <Luigi.Selmi@iais.fraunhofer.de>
2016-10-12 17:00:02 +0200

    fixed typo

--- start_workbench.sh
+++ start_workbench.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-./bin/silk-workbench -Dworkspace.provider.file.dir=/home/lidakra/mapping/Workspace -Dhttp.port=9005 -Dparser.text.maxLength=1024k -Dapplication.context=/silk/ 
+./bin/silk-workbench -Dworkspace.provider.file.dir=/home/lidakra/mapping/Workspace -Dhttp.port=9005 -Dparsers.text.maxLength=1024k -Dapplication.context=/silk/ 
 
