0fb7f239d5559c595c33b226a6082f953b4f399f
camilomorales <camilo.morales@gmail.com>
2017-05-24 14:51:07 +0200

    Add configuration parameters for the new version of Silk.

--- start_workbench.sh
+++ start_workbench.sh
@@ -3,5 +3,4 @@
 find . -type f -name RUNNING_PID -exec rm -f {} \;
 
 # Start Silk
-./bin/silk-workbench -Dworkspace.provider.file.dir=/home/lidakra/mapping/Workspace -Dhttp.port=9005 -Dparsers.text.maxLength=1024k -Dapplication.context=/silk/ 
-
+./bin/silk-workbench -Dworkspace.provider.file.dir=/home/lidakra/mapping/Workspace -Dworkspace.repository.projectFile.dir=/home/lidakra/mapping/Workspace -Dworkspace.repository.plugin=projectFile -Dhttp.port=9005 -Dparsers.text.maxLength=1024k -Dapplication.context=/silk/
